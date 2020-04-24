7f52cc420ddfbaa829e6d3ef0fd224035b884242
John DoeMujdat <elieyioglumujdat@gmail.com>
2020-04-24 08:51:50 +0300

    Updates

---
+++ precache-manifest.d711ddec8c610adcce7b29808037c4cf.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "266d7421916786750690c1d896b4b48f",
+    "url": "/citytours/index.html"
+  },
+  {
+    "revision": "b660ab501e0d12f579e8",
+    "url": "/citytours/static/css/2.8d24c2a2.chunk.css"
+  },
+  {
+    "revision": "12ee1c18fbdcf2d52faa",
+    "url": "/citytours/static/css/main.8bfd4bb6.chunk.css"
+  },
+  {
+    "revision": "b660ab501e0d12f579e8",
+    "url": "/citytours/static/js/2.6d68dc46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/citytours/static/js/2.6d68dc46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12ee1c18fbdcf2d52faa",
+    "url": "/citytours/static/js/main.8485fc43.chunk.js"
+  },
+  {
+    "revision": "00e32a5731d3af9fd255",
+    "url": "/citytours/static/js/runtime-main.5ece6043.js"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/citytours/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/citytours/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/citytours/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/citytours/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/citytours/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/citytours/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/citytours/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/citytours/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/citytours/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/citytours/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/citytours/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/citytours/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/citytours/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/citytours/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/citytours/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "12a6659ff5467c71b599841286fb8e7f",
+    "url": "/citytours/static/media/logo.12a6659f.svg"
+  }
+]);
